63751a3920abb2dcbcc3021e5517b1598ae91bad
nayunora <117896579+nayunora@users.noreply.github.com>
2022-12-04 12:37:57 +0900

    main.blocks, main.ts 업데이트하기

--- main.ts
+++ main.ts
@@ -1,8 +1,10 @@
 function cold () {
     strip.showColor(neopixel.colors(NeoPixelColors.Black))
-    range1.showRainbow(0, 179)
-    range2.showRainbow(180, 310)
-    basic.pause(5000)
+    while (light2 == 2) {
+        range1.showRainbow(0, 179)
+        range2.showRainbow(180, 310)
+    }
+    basic.pause(100)
 }
 function off () {
     strip.showColor(neopixel.colors(NeoPixelColors.Black))
@@ -10,7 +12,9 @@ function off () {
 }
 function warm () {
     strip.showColor(neopixel.colors(NeoPixelColors.Black))
-    range1.showRainbow(0, 179)
+    while (light2 == 1) {
+        range1.showRainbow(0, 179)
+    }
     basic.pause(100)
 }
 function orange () {
@@ -23,8 +27,9 @@ let pressure_sensor = 0
 let range2: neopixel.Strip = null
 let range1: neopixel.Strip = null
 let strip: neopixel.Strip = null
-let light1 = 0
 let light2 = 0
+light2 = 0
+let light22 = 0
 let mode = 0
 strip = neopixel.create(DigitalPin.P16, 16, NeoPixelMode.RGB)
 range1 = strip.range(0, 8)
@@ -52,17 +57,21 @@ basic.forever(function () {
     touch_sensor1 = pins.analogReadPin(AnalogPin.P4)
     if (mode == 1 && touch_sensor1 > 180) {
         warm()
+        light2 = 1
     }
     touch_sensor2 = pins.analogReadPin(AnalogPin.P10)
     if (mode == 1 && touch_sensor2 > 180) {
         cold()
+        light2 = 2
     }
     if (mode == 2 && pressure_sensor < 700) {
         off()
         mode = 0
+        light2 = 0
     }
     if (mode == 1 && pressure_sensor > 600) {
         off()
         mode += 1
+        light2 = 0
     }
 })
